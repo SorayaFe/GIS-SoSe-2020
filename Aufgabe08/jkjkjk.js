84909db6701d9b41551e2be65bbaa0e3399e051d
Soraya Ferdani <soraya.ferdani@googlemail.com>
2020-06-17 17:50:25 +0200

    Aufgabe08

--- Aufgabe08/jkjkjk.js
+++ Aufgabe08/jkjkjk.js
@@ -5,13 +5,13 @@ var Aufgabe08;
     senden?.addEventListener("click", handleSenden);
     async function handleSenden() {
         let formData = new FormData(document.forms[0]);
+        let url = "https://gispraktikum2020.herokuapp.com/";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
-        let url = "https://gispraktikum2020.herokuapp.com/";
         url = url + "?" + query.toString();
         let antwort = await fetch(url);
-        let antwort2 = antwort.url;
-        console.log(antwort2);
+        let lkjlj = antwort.url;
+        console.log(lkjlj);
         for (let entry of query) {
             console.log(entry);
             console.log("name: " + entry[0]);
@@ -19,4 +19,4 @@ var Aufgabe08;
         }
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
-//# sourceMappingURL=script.js.map
+//# sourceMappingURL=jkjkjk.js.map
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -3,26 +3,25 @@
     let senden: HTMLElement = <HTMLElement>document.getElementById("senden");
     senden?.addEventListener("click", handleSenden);
 
-    let formData: FormData;
-
     async function handleSenden(): Promise<void> {
-        funktion(await funktion2());
+
+        let formData: FormData = new FormData(document.forms[0]);
+
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        
+        let url: string = "https://gispraktikum2020.herokuapp.com/";
+
+        url = url + "?" + query.toString();
+
+        let antwort: Response = await fetch(url);
+        let antwort2: string = antwort.url;
+        console.log(antwort2);
+
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
     }
-
-    async function funktion2(): Promise<string> {
-
-     formData = new FormData(document.forms[0]);
-     let url: string = "https://gispraktikum2020.herokuapp.com/";
-     let query: URLSearchParams = new URLSearchParams(<any>formData);
-     url = url + "?" + query.toString();
-     return url;
-    }
-
-    async function funktion(url: RequestInfo): Promise<void> {
-    
-    let antwort: Response = await fetch(url);
-    let kjll: string = await antwort.text();
-    console.log(kjll);
-
-    }
-}
+ }
