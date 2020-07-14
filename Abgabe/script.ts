ca85898d23e7b69331b76ed52de4efcf1f9a11ae
Soraya Ferdani <soraya.ferdani@googlemail.com>
2020-07-14 13:30:27 +0200

    Abg

---
+++ Abgabe/script.ts
@@ -0,0 +1,10 @@
+namespace Abgabe{
+
+    export interface Optionen {
+        _bild: string;
+        _bildAlt: string;
+        _name: string;
+        _preis: number;
+        _kategorie: string
+    }
+}
